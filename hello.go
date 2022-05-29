b2d42cf9c338f929399e5b9ceaa0a6704db98fe3
Abdulrasheed Fawole <fawomath@gmail.com>
2022-05-29 21:20:13 +0100

    some change in name

--- hello.go
+++ hello.go
@@ -1,28 +1,5 @@
 package main
 
-import "fmt"
-
 func main() {
-	xs := []float64{98, 93, 77, 82, 83}
-	fmt.Println(average(xs))
-	fmt.Println(add(xs...))
-	x := 0
-	increment := func() int {
-		x++
-		return x
-	}
-	fmt.Println(increment())
-	fmt.Println(increment())
-
-	nextEven := makeEvenGenerator()
-	fmt.Println(nextEven())
-	fmt.Println(nextEven())
-	fmt.Println(nextEven())
-
-	nextOdd := makeOddGenerator()
-	fmt.Println(nextOdd())
-	fmt.Println(nextOdd())
-	fmt.Println(nextOdd())
-	fmt.Println(factorial(5))
 
 }
