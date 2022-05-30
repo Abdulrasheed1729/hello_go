5962786202789ebe8b72b7d5cfc4626b7b56d6bf
Abdulrasheed Fawole <fawomath@gmail.com>
2022-05-30 13:30:14 +0100

    struct

--- hello.go
+++ hello.go
@@ -1,12 +1,24 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
+
+type Shape interface {
+	area() float64
+}
+type Person struct {
+	Name string
+}
+
+func (p *Person) Talk() {
+	fmt.Println("Hi, my name is", p.Name)
+}
+
+type Android struct {
+	Person
+	Model string
+}
 
 func main() {
-	c := Circle{0, 0, 5}
-	fmt.Println(c.area())
-	r := Rectangle{0, 0, 10, 10}
-	fmt.Println(r.area())
+	a := new(Android)
+	a.Talk()
 }
