29cc92c71a3288e59d5399137bee44a0dff77902
Abdulrasheed Fawole <fawomath@gmail.com>
2022-05-30 09:24:25 +0100

    yet an other commit

--- hello.go
+++ hello.go
@@ -1,5 +1,37 @@
 package main
 
-func main() {
+import "fmt"
 
+func half(x int) (int, bool) {
+	return x / 2, x%2 == 0
+}
+
+func greatest(args ...int) int {
+	var max int = 0
+	for _, v := range args {
+		if v == 0 || v > max {
+			max = v
+		}
+	}
+	return max
+}
+
+func swap(x, y *int) {
+	*x, *y = *y, *x
+
+}
+
+func square(x *float64) {
+	*x = *x * *x
+}
+
+func main() {
+	w := 1.5
+	square(&w)
+	x, y := 5, 6
+	swap(&x, &y)
+
+	fmt.Println(half(9))
+	fmt.Println(x, y)
+	fmt.Println(w)
 }
--- readme.md
+++ readme.md
@@ -1,1 +1,3 @@
 #### Reviving my go Skills
+
+Please don't my the file structure, this repo is just for keeping random [Go](go.dev) files 
